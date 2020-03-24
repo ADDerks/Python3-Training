04c3ae1443b3c62ad4b75435924d158b416d1e71
ADDerks <Anthony.Derks@gmail.com>
2020-03-23 21:04:22 -0400

    Create python_notes.py

---
+++ python_notes.py
@@ -0,0 +1,15 @@
+# python course notes and examples.
+# to write a function, define the function, then use the name of function.
+# Example: def function_name():
+			#(Tab) some code...
+
+# example
+
+def greet_customer():
+	print("Welcome to Engrossing Grocers.")
+	print("Today's Special is mandarin oranges.")
+	print("Have fun shopping!")
+
+greet_customer()
+#prints greeting lines.
+
